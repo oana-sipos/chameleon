d331cf42f749998ca2a8cac7f90bf5ad69da12c0
Oana Sipos <oanasipos@gmail.com>
2014-11-11 20:54:56 +0100

    Cleanup

--- db/migrate/20141111171949_create_events_users_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateEventsUsersJoinTable < ActiveRecord::Migration
-  def change
-    create_join_table :users, :events do |t|
-      t.index [:user_id, :event_id]
-      t.index [:event_id, :user_id]
-    end
-  end
-end
